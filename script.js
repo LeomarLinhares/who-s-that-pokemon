14089b33168f5ce708fd15522c4687fb3bdf472f
vinicius-mns <vinicius_mns@hotmail.com>
2021-08-25 11:27:10 -0300

    fav icon / pokebola / leds

--- script.js
+++
@@ -1,6 +0,0 @@
-const botao = document.querySelector('.btn');
-const pokemon = document.querySelector('.pokemon')
-
-botao.addEventListener('click', () => {
-  pokemon.classList.remove('secret')
-})
